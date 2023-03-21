75e8c95e0b33d7327fdf37dad855638584d4fc35
diphone-net <43610108+diphone-net@users.noreply.github.com>
2023-03-21 16:06:48 +0100

    initial commit

---
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/tpr/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.udl.gtidic.course2223.teacher.tpr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
